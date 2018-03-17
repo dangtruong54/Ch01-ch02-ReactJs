392b8665476552c3167925e5f86f6ecf57f30cb7
DESKTOP-N37IO01\TruongDX <truongdx54@gmail.com>
2018-03-17 15:50:59 +0700

    Commit

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,18 +1,71 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="row">
+            
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+              
+              <div className="panel panel-info">
+                  <div className="panel-heading">
+                    <h3 className="panel-title">React Js</h3>
+                  </div>
+                  <div className="panel-body">
+                    
+                    <ul className="list-group">
+                      <li className="list-group-item">Item 1</li>
+                      <li className="list-group-item">Item 2</li>
+                      <li className="list-group-item">Item 3</li>
+                    </ul>
+                    
+                  </div>
+              </div>
+              
+            </div>
+
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+              
+                <div className="panel panel-info">
+                    <div className="panel-heading">
+                      <h3 className="panel-title">React Js</h3>
+                    </div>
+                    <div className="panel-body">
+                      
+                      <ul className="list-group">
+                        <li className="list-group-item">Item 1</li>
+                        <li className="list-group-item">Item 2</li>
+                        <li className="list-group-item">Item 3</li>
+                      </ul>
+                      
+                    </div>
+                </div>
+                
+              </div>
+
+              <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+              
+                  <div className="panel panel-info">
+                      <div className="panel-heading">
+                        <h3 className="panel-title">React Js</h3>
+                      </div>
+                      <div className="panel-body">
+                        
+                        <ul className="list-group">
+                          <li className="list-group-item">Item 1</li>
+                          <li className="list-group-item">Item 2</li>
+                          <li className="list-group-item">Item 3</li>
+                        </ul>
+                        
+                      </div>
+                  </div>
+                  
+                </div>                  
+              
+            </div>
       </div>
     );
   }
---
+++ ch01/src/components/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div className="App">      
+      <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+        
+      </div>
+      
+        Hello World
+      </div>
+    );
+  }
+}
+
+export default Hello;

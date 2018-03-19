cdeb1cc2789d4558abc00e84414c031fd530599e
truongdx <truongdx@beetsoft.com.vn>
2018-03-19 09:48:45 +0700

    bai 10

--- ch01/src/App.js
+++ ch01/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Course from '../src/components/Course'
 //import logo from './logo.svg';
 import './App.css';
 
@@ -7,65 +8,8 @@ class App extends Component {
     return (
       <div className="App">
         <div className="row">
-            
-            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-              
-              <div className="panel panel-info">
-                  <div className="panel-heading">
-                    <h3 className="panel-title">React Js</h3>
-                  </div>
-                  <div className="panel-body">
-                    
-                    <ul className="list-group">
-                      <li className="list-group-item">Item 1</li>
-                      <li className="list-group-item">Item 2</li>
-                      <li className="list-group-item">Item 3</li>
-                    </ul>
-                    
-                  </div>
-              </div>
-              
-            </div>
-
-            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-              
-                <div className="panel panel-info">
-                    <div className="panel-heading">
-                      <h3 className="panel-title">React Js</h3>
-                    </div>
-                    <div className="panel-body">
-                      
-                      <ul className="list-group">
-                        <li className="list-group-item">Item 1</li>
-                        <li className="list-group-item">Item 2</li>
-                        <li className="list-group-item">Item 3</li>
-                      </ul>
-                      
-                    </div>
-                </div>
-                
-              </div>
-
-              <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-              
-                  <div className="panel panel-info">
-                      <div className="panel-heading">
-                        <h3 className="panel-title">React Js</h3>
-                      </div>
-                      <div className="panel-body">
-                        
-                        <ul className="list-group">
-                          <li className="list-group-item">Item 1</li>
-                          <li className="list-group-item">Item 2</li>
-                          <li className="list-group-item">Item 3</li>
-                        </ul>
-                        
-                      </div>
-                  </div>
-                  
-                </div>                  
-              
-            </div>
+          <Course />                        
+        </div>
       </div>
     );
   }
--- ch01/src/components/Course.js
+++ ch01/src/components/Course.js
@@ -3,8 +3,21 @@ import React, { Component } from 'react';
 class Course extends Component {
   render() {
     return (
-      <div className="App">      
-        <h3>Course Component</h3>
+      <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">              
+        <div className="panel panel-info">
+            <div className="panel-heading">
+              <h3 className="panel-title">React Js</h3>
+            </div>
+            <div className="panel-body">
+              
+              <ul className="list-group">
+                <li className="list-group-item">Item 1</li>
+                <li className="list-group-item">Item 2</li>
+                <li className="list-group-item">Item 3</li>
+              </ul>
+              
+            </div>
+        </div>              
       </div>
     );
   }

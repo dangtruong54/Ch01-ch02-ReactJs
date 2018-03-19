cf4dfab0c9d486540ce1ad464f7e4d92befa5db2
truongdx <truongdx@beetsoft.com.vn>
2018-03-19 15:29:30 +0700

    bai 13 event

--- ch01/src/components/Course.js
+++ ch01/src/components/Course.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 import Lesson from './Lesson';
 
 class Course extends Component {
+
+  constructor(props) {
+    super(props);
+    this.handleClick3 = this.handleClick3.bind(this);
+  }
+
+  handleClick1() {
+    alert("View 1");
+  }
+
+  handleClick2(content) {
+    alert(content);
+  }
+
+  handleClick3() {
+    alert(this.props.name);
+  }
+
   showButtonFree(){
     const isFree = this.props.free;
     if(isFree === true) {
       return <div className="panel-footer">
-                <button type="button" className="btn btn-warning">View</button>
+                <div className="btn-group">
+                  <button onClick={this.handleClick1} type="button" className="btn btn-warning">View 1</button>
+                  <button onClick={() => this.handleClick2("View 2")} type="button" className="btn btn-danger">View 2</button>
+                  <button onClick={this.handleClick3} type="button" className="btn btn-success">View 3</button>
+                </div>
              </div>
     }
   }
+
   render() {
     return (
       <div>

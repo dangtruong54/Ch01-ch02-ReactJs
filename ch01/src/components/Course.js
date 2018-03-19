d9b2091ef32c5dc76325c707fcadc82703da1079
DESKTOP-N37IO01\TruongDX <truongdx54@gmail.com>
2018-03-19 07:32:06 +0700

    Commit

---
+++ ch01/src/components/Course.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Course extends Component {
+  render() {
+    return (
+      <div className="App">      
+        <h3>Course Component</h3>
+      </div>
+    );
+  }
+}
+
+export default Course;

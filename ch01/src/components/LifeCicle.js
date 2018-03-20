19feedea20cd857c2bac0ec7e64c253b6f4ebf1e
truongdx <truongdx@beetsoft.com.vn>
2018-03-20 09:26:20 +0700

    Tim hieu ve lifeClycle

---
+++ ch01/src/components/LifeCicle.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import $ from 'jquery'; 
+class LifeCicle extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title : "LifeCicle"
+    };
+    this.handleChangeTitle = this.handleChangeTitle.bind(this);
+    console.log("constructor")
+  }
+
+  handleChangeTitle() {
+    this.setState({
+      title : "Changed Title when using LifeCicle"
+    }) 
+  }
+
+  componentWillMount(){
+    console.log("componentWillMount");
+    this.setState({
+      title : "Changed Title WillMount when using LifeCicle"
+    }) 
+  }
+
+  componentDidMount(){
+    console.log("componentDidMount");
+    $("h3.panel-title").css('color', 'white');
+    this.setState({
+      title : "Changed Title component DidMount when using LifeCicle"
+    }) 
+  }
+
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">              
+          <div className="panel panel-info">
+              <div className="panel-heading">
+                <h3 className="panel-title">{this.state.title}</h3>
+              </div>
+              <div className="panel-body">
+                <p><button onClick={this.handleChangeTitle} type="button" className="btn btn-success">Toggle Outline</button></p>           
+              </div>     
+          </div>                     
+        </div>
+      </div>           
+     
+    );
+  }
+}
+
+export default LifeCicle;
--- ch01/src/index.js
+++ ch01/src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 //import Hello from './components/Hello';
+import LifeCicle from './components/LifeCicle'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LifeCicle />, document.getElementById('root'));
 registerServiceWorker();

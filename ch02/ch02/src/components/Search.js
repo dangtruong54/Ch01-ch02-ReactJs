9d7eed08ca0c246dcdf189c09dd989103fcc0b88
truongdx <truongdx@beetsoft.com.vn>
2018-03-21 17:39:24 +0700

    Search Task

--- ch02/ch02/src/components/Search.js
+++ ch02/ch02/src/components/Search.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 
 class Search extends Component {
-  render() {
-    return ( 
-        <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
-            <div className="input-group">
-                <input type="text" className="form-control" ref="search" placeholder="Search for..." />
-                <span className="input-group-btn">
-                    <button className="btn btn-info" type="button">Go!</button>
-                </span>
+    render() {
+        return ( 
+            <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div className="input-group">
+                    <input type="text" className="form-control" ref="search" placeholder="Search for..." />
+                    <span className="input-group-btn">
+                        <button className="btn btn-info" type="button">Go!</button>
+                        <button className="btn btn-warning" type="button">Clear</button>
+                    </span>
+                </div>
             </div>
-        </div>
-    );
-  }
+        );
+    }
 }
 
 export default Search;

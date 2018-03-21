f78281c315dc43b2670706a179df0e8ce1066bd7
truongdx <truongdx@beetsoft.com.vn>
2018-03-21 10:48:38 +0700

    Finish display data

--- ch02/ch02/src/components/Item.js
+++ ch02/ch02/src/components/Item.js
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 
 class Item extends Component {
-  render() {
+    
+    showLevel(level) {
+        let elemLevel = null;
+        if(level === 0){
+            elemLevel = <span className="label label-default">Small</span>;
+        }else if(level === 1){
+            elemLevel = <span className="label label-info">Nomal</span>;
+        }else if(level === 2){
+            elemLevel = <span className="label label-danger">High</span>;
+        }
+        return elemLevel;
+    }
 
-    return (  
-        <tr>
-            <td className="text-center">{this.props.index + 1}</td>
-            <td>{this.props.item.name}</td>
-            <td className="text-center"><span className="label label-danger">{this.props.item.level}</span></td>
-            <td>
-                <button type="button" className="btn btn-warning">Edit</button>
-                <button type="button" className="btn btn-danger">Delete</button>
-            </td>
-        </tr>
-    );
-  }
+    render() {
+        const {item} = this.props;
+        const {index} = this.props;
+       
+        return (  
+            <tr>
+                <td className="text-center">{index + 1}</td>
+                <td>{item.name}</td>
+                <td className="text-center">{this.showLevel(item.level)}</td>
+                <td>
+                    <button type="button" className="btn btn-warning">Edit</button>
+                    <button type="button" className="btn btn-danger">Delete</button>
+                </td>
+            </tr>
+        );
+    }
 }
 
 export default Item;
